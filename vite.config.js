a924b72cf97948c1d663ddeee2878491ce5f2d78
Sophia de Jong <Sophia@Sophia-Marina-de-Jong-3.local>
2022-10-18 10:18:26 +0200

    viteconfig

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  // Carpeta donde creare el build de yarn
+  build: { outDir: 'docs'},
+  // Comprobaos si estamos en produccion yla case es el nombre del repositorio
+  base: process.env.NODE_ENV === 'produccion' ? '/final-project' : '/'
 })

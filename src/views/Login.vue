ac02a63ca1a696abb0eed4b2202d862bce02da24
Sophia de Jong <Sophia@Sophia-Marina-de-Jong-3.local>
2022-10-19 13:07:12 +0200

    piniapersist

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,20 @@
 <template>
-</template>
-<script setup>
-</script>
-<style scoped>
-</style>
+    <section class="sm:bg-grayfons h-max min-h-screen"> <!--  class="bg-gray-200 h-screen 	align-items: center" -->
+        <router-view></router-view>
+    </section>
+  </template>
+  
+  <script setup>
+    import SignIn from './components/SignIn.vue'
+    import SignUp from './components/SignUp.vue'
+    //hacer un evento para el enter que haga "submit" @keyup tecla enter etc (buscar)
+    import { ref } from 'vue';
+  
+    let route = ref("/auth/sign-up");
+    let buttonText = "Sign Up";
+  
+  
+  </script>
+  
+  <style>
+  </style>
